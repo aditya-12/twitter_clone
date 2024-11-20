e734ad6ebc6cca5603d581574b0d2897f35fadc5
aditya-12 <adityakaushik2011@gmail.com>
2024-11-20 11:51:15 -0600

    1. AuthController added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { authCheck, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/signup", signup);
+router.post("/signup", signup);
 
-router.get("/login", login);
+router.post("/login", login);
 
-router.get("/logout", logout);
+router.post("/logout", logout);
 
+router.get("/authCheck", protectRoute, authCheck);
 
 export default router;
