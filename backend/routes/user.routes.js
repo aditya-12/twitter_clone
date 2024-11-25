247ca9b7d1628a2fc2a723e972fe6dfe3c5f281f
aditya-12 <adityakaushik2011@gmail.com>
2024-11-25 11:15:32 -0600

    1. user routes and controllers added

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
+
+export default router;
+
+// export default userRoutes = async(req,res) => {
+//     try {
+        
+//     } catch (error) {
+        
+//     }
+// }
