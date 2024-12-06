c440ac4a125c2b2feabee1fc6706a959518a43bc
aditya-12 <adityakaushik2011@gmail.com>
2024-12-06 11:41:08 -0600

    1. corrected getUserProfile function was showing password earlier

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -7,7 +7,7 @@ import User from "../models/user.model.js";
 export const getUserProfile = async(req,res) => {
     const {username} = req.params;
     try {
-        const user = await User.findOne({username}).select("-passwprd");
+        const user = await User.findOne({username}).select("-password");
         if (!user) {
             return res.status(404).json({message: "User not found"});
         }
