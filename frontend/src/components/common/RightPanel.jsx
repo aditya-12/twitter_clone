e73fe738be0a59dd9099ef47cb7a4faf39e66467
aditya-12 <adityakaushik2011@gmail.com>
2024-12-30 15:42:42 -0600

    3. use Follow Hook Added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-// import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
 import { useQuery } from "@tanstack/react-query";
+// import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+
+import useFollow from "../hooks/useFollow";
+
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
 	// const isLoading = false;
@@ -19,6 +23,8 @@ const RightPanel = () => {
 			}
 		}
 	});
+
+	const {follow, isPending} = useFollow();
 
 	if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
 
@@ -59,9 +65,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) =>{
+											 e.preventDefault()
+											 follow(user._id);
+											}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner/> : "Follow"}
 									</button>
 								</div>
 							</Link>
